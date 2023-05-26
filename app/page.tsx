9a66db3338900c47e70a278d10d2f1a355ab0537
Tyler Nichols <nicholsty72@gmail.com>
2023-05-26 12:44:24 -0500

    solved the props typescript error for the json object and printing the component properly, slight css tweaks from mobile and then gtg

--- app/page.tsx
+++ app/page.tsx
@@ -3,34 +3,25 @@ import Summary from '../components/summaryItems'
 export default function Home() {
   return (
     <main className="h-screen bg-slate-100 flex justify-center items-center">
-      <div className="grid grid-cols-2  border-1 border-transparent rounded-xl w-8/12">
+      <div className="grid grid-cols-2 md:grid-cols-1 sm:grid-cols-1 border-1 border-transparent rounded-xl w-8/12">
         <div className="bg-gradient-to-t from-indigo-700 from-40% to-indigo-500 border-1 border-transparent rounded-xl flex flex-col items-center py-8 px-12">
           <h1 className="text-lg text-slate-300">Your Result</h1>
           <div className="rounded-full bg-indigo-700 flex flex-col px-16 py-14 my-2">
-            <h2 className="text-2xl text-center">76</h2>
-            <h4 className="text-xs text-slate-300">of 100</h4>
+            <h2 className="text-7xl text-center">76</h2>
+            <h4 className="text-xs text-center text-slate-300">of 100</h4>
           </div>
           <h3 className="text-lg py-2">Great</h3>
-          <p className="text-xs text-slate-300 text-center">
+          <p className="text-xs text-slate-300 text-center px-5">
             You scored higher than 65% of the people who have taken these tests.
           </p>
         </div>
         <div className="rounded-xl flex flex-col shadow-2xl p-8">
           <h3 className="text-lg text-black pb-4">Summary</h3>
-          <Summary
-          id={1}
-          />
-          <div className="flex bg-red-100 p-4 my-2 justify-between">
-            <div>
-              <h4 className="text-md text-red-400">Reaction</h4>
-            </div>
-            <div>
-              <h4 className="text-md text-black">
-                80 <span className="text-slate-500">/ 100</span>
-              </h4>
-            </div>
-          </div>
-          <button className="bg-gray-800">Continue</button>
+          <Summary id={1} color={'red'} />
+          <Summary id={2} color={'yellow'} />
+          <Summary id={3} color={'green'} />
+          <Summary id={4} color={'blue'} />
+          <button className="bg-gray-800 mt-2 rounded-full py-3">Continue</button>
         </div>
       </div>
     </main>
--- components/summaryItems.tsx
+++ components/summaryItems.tsx
@@ -3,42 +3,50 @@
 import Image from "next/image";
 import Scores from "../app/api/data.json";
 
-export default function Summary(props:number) {
-  console.log(Scores);
+interface ID {
+  id: number;
+  color: string;
+}
+interface TestID {
+  id: number;
+  category: string;
+  score: number;
+  icon: string;
+}
 
-  interface ID {
-    id: 1;
-  }
-  interface Score {
-    score: 1
-  }
-  interface ScoreType {
-      category: "test";
-      score: Score;
-      icon: "image";
-      [key: number]: ID;
+interface TestScore extends Array<TestID> {}
+
+export default function Summary({ id, color }: ID) {
+  function filterById(jsonObject: TestScore, id: number) {
+    return jsonObject.filter(function (jsonObject) {
+      return jsonObject["id"] == id;
+    })[0];
   }
 
-  const typeScore: ScoreType = Scores["scores"];
+  var selectedObject = filterById(Scores["scores"], id);
 
   return (
-    <>
-      {Object.entries(typeScore).forEach((score) => {
-        <div key={score["id"]}>
-          <Image
-            src={score["icon"]}
-            height={200}
-            width={200}
-            alt="Score category Image"
-          />
+    <div
+      className={`flex bg-${color}-100 p-4 my-2 justify-between rounded-xl`}
+      key={selectedObject.id}
+    >
+      <div className="flex items-center">
+        <Image
+          src={selectedObject.icon}
+          height={30}
+          width={30}
+          alt="Score category Image"
+        />
 
-          <h2>{score["category"]}</h2>
-          <h3>
-            {score["score"]}
-            <span>/100</span>
-          </h3>
-        </div>;
-      })}
-    </>
+        <h4 className={`text-md text-${color}-600 pl-2`}>
+          {selectedObject.category}
+        </h4>
+      </div>
+      <div className="flex items-center">
+        <h4 className="text-md text-black pr-1">{selectedObject.score}</h4>
+
+        <span className="text-slate-500">/ 100</span>
+      </div>
+    </div>
   );
 }
